b6d2a1ff6a6f0809298cce2d8eef48e18a02fb76
lutsiuck <vlutsyck@gmail.com>
2021-03-09 23:56:39 +0200

    marge

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const Post = (props) => {
-    return (
-        <div className="col-md-6 mb-2">
-            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                <div className="col p-4 d-flex flex-column position-static">
-                    <strong className="d-inline-block mb-2 text-primary">World</strong>
-                    <h3 className="mb-0">{props.post.title}</h3>
-                    <div className="mb-1 text-muted">{props.post.date}</div>
-                    <p className="card-text mb-auto">{props.post.desc}</p>
-                    <a href="#" className="stretched-link">Continue reading</a>
-                </div>
-                <div className="col-auto d-none d-lg-block">
-                <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                </div>
-            </div>
+  return (
+    <div className="col-md-6 mb-2">
+      <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div className="col p-4 d-flex flex-column position-static">
+          <strong className="d-inline-block mb-2 text-primary">World</strong>
+          <h3 className="mb-0">{props.post.title}</h3>
+          <div className="mb-1 text-muted">{props.post.date}</div>
+          <p className="card-text mb-auto">{props.post.desc}</p>
+          <a href="#" className="stretched-link">
+            Continue reading
+          </a>
         </div>
-    )
-}
+        <div className="col-auto d-none d-lg-block">
+          <svg
+            className="bd-placeholder-img"
+            width="200"
+            height="250"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: Thumbnail"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#55595c"></rect>
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+              Thumbnail
+            </text>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Post;
